75f92f168c9e70fbc2ef46d6a03210381ca65354
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-08-08 18:57:01 +0300

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,18 +1,19 @@
 var rpc = require("discord-rpc")
 const client = new rpc.Client({ transport: 'ipc' })
 client.on('ready', () => {
-    console.log("ArviS#0011 - Discord Özel RPC => RPC'niz Hazır, iyi Günlerde Kullanın."),
+    console.log("ArviS - Discord Özel RPC => RPC'niz Hazır, iyi Günlerde Kullanın."),
 client.request('SET_ACTIVITY', { 
 pid: process.pid, 
 activity : {
-details : "❤️・Stranger Things Türkiye",  // Başlığın altında gözüken ince fontlu yazı kısmı.
+timestamps: { start: Date.now() },
+details : "❤️・Discord",  // Başlığın altında gözüken ince fontlu yazı kısmı.
 assets : {
-large_image : "",  // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
-small_image : "", // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
+large_image : "arvis",  // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
+small_image : "mavitik", // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
 large_text : "ArviS", // Botu oluştururken girdiğiniz ismi yazın.
 },
-buttons : [{label : "Stranger Things (Discord)" , url : "https://discord.gg/yYbFZPhJmg"},{label : "Stranger Things (İnstagram)" , url : "https://instagram.com/strangerturkiyedc"}]
+buttons : [{label : "Discord" , url : "https://discord.gg/dcgo"},{label : "Instagram" , url : "https://instagram.com/arvis_here"}]
 }
 })
 })
-client.login({ clientId : "" }).catch(console.error); //clientID bölümüne kendi Bot (Application) ID'ni yazacaksınız.
+client.login({ clientId : "989486756762116117" }).catch(console.error); //clientID bölümüne kendi Bot (Application) ID'ni yazacaksınız.
