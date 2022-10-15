2f5dad45f0f08632a888325de3f41e225eaaa271
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-10-16 00:22:18 +0300

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,19 +1,25 @@
 var rpc = require("discord-rpc")
 const client = new rpc.Client({ transport: 'ipc' })
+
 client.on('ready', () => {
     console.log("ArviS - Discord Özel RPC => RPC'niz Hazır, iyi Günlerde Kullanın."),
+
 client.request('SET_ACTIVITY', { 
 pid: process.pid, 
 activity : {
-timestamps: { start: Date.now() },
-details : "❤️・Discord",  // Başlığın altında gözüken ince fontlu yazı kısmı.
+details : "❤️・@AngelDevelopers",  // Başlığın altında gözüken ince fontlu yazı kısmı.
 assets : {
-large_image : "arvis",  // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
-small_image : "mavitik", // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
+timestamps: { start: Date.now() },
+
+large_image : "arvislogo",  // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
+small_image : "bluetik", // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
 large_text : "ArviS", // Botu oluştururken girdiğiniz ismi yazın.
 },
-buttons : [{label : "Discord" , url : "https://discord.gg/dcgo"},{label : "Instagram" , url : "https://instagram.com/arvis_here"}]
+
+buttons : [{label : "Discord" , url : "https://discord.gg/5cZ5BaZBCr"},{label : "Instagram" , url : "https://instagram.com/arvis_here"}]
+
 }
 })
 })
-client.login({ clientId : "989486756762116117" }).catch(console.error); //clientID bölümüne kendi Bot (Application) ID'ni yazacaksınız.
+
+client.login({ clientId : "1030947981492502598" }).catch(console.error); //clientID bölümüne kendi Bot (Application) ID'ni yazacaksınız.
