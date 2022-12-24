d5918cb21d1a37bfa9ba094cf0e99e207c0d7693
ArviS#0011 <69751083+ArviSlayer@users.noreply.github.com>
2022-12-24 16:36:54 +0300

    Add files via upload

--- ccd.js
+++ ccd.js
@@ -1,10 +1,11 @@
-var rpc = require("discord-rpc")
-const client = new rpc.Client({ transport })
+﻿var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
 
 client.on('ready', () => {
-    console.log("ArviS - Discord Özel RPC => RPC'niz Hazır, iyi Günlerde Kullanın."),
+    console.log("ArviS#0011 - Discord Özel RPC => RPC'niz Hazır, iyi Günlerde Kullanın"),
 
-client.request(, { 
+client.request('SET_ACTIVITY', { 
+pid: process.pid, 
 activity : {
 timestamps: { start: Date.now() },
 details : "❤️・Software",  // Başlığın altında gözüken ince fontlu yazı kısmı.
@@ -12,10 +13,12 @@ assets : {
 
 large_image : "arvislogo",  // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
 small_image : "bluetik", // Rich Presence kısmına koyduğunuz fotoğrafın adını yazın.
-large_text : "ArviS", // Botu oluştururken girdiğiniz ismi yazın.
+
+large_text : "Made by ❤️ ArviS#0011", //Büyük Resim Text
+small_text: "Discorder", //Küçük Resim Text
 },
 
-buttons : [{label : "Discord" , url : "https://discord.gg/npJ3t2MNVZ"},{label : "Instagram" , url : "https://instagram.com/arvis_here"}]
+buttons : [{label : "Mi' Amor", url: "https://github.com/ArviSlayer"}]
 
 }
 })
